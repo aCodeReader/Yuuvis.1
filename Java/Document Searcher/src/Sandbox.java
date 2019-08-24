bceeb886057db3ee26e7b1c9266b8fd2302392f5
Jeff Pape <jeffpape@discovery.local>
2019-08-24 17:30:15 -0500

    corrected line header increment

--- Java/Document Searcher/src/Sandbox.java
+++ Java/Document Searcher/src/Sandbox.java
@@ -13,6 +13,12 @@ public class Sandbox
 		String line = s.nextLine();
 		while (!line.equalsIgnoreCase("syllabus")) {
 			headers.add(line);
+			if (s.hasNextLine()) {
+				line = s.nextLine();
+			}
+			else {
+				break;
+			}
 		}
 				
 		String judge = headers.get(0);
@@ -27,7 +33,7 @@ public class Sandbox
 		String pageNumbers = headers.get(3);
 		String argued = headers.get(4);
 		String decided = headers.get(5);
-		String courtsInvolved = headers.get(7) + headers.get(8);
+		String courtsInvolved = headers.get(7) + "; " + headers.get(8);
 		
 		ArrayList<String> details = new ArrayList<String>();
 		
@@ -48,10 +54,11 @@ public class Sandbox
 				);
 		return ret;
 	}
-	public static void main(String[] args) 
+	public static void main(String[] args) throws FileNotFoundException 
 	{
 		// TODO Auto-generated method stub
-
+		Case a_case = readFile(new File("case_1.txt"));
+		System.out.println(a_case);
 	}
 
 }
